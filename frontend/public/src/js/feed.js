3cb2c11a52a18899c0ca3a572e4a97637908863e
AntoniaSperling <antonia.sperling@stundent.htw.-berlin.de>
2023-09-20 23:27:51 +0200

    indexDB backend-anbindung

--- frontend/public/src/js/feed.js
+++ frontend/public/src/js/feed.js
@@ -4,43 +4,66 @@ let closeCreatePostModalButton = document.querySelector('#close-create-post-moda
 let sharedMomentsArea = document.querySelector('#shared-moments');
 
 function openCreatePostModal() {
-  createPostArea.style.display = 'block';
+  createPostArea.style.visibility = 'visible';
 }
 
 function closeCreatePostModal() {
-  createPostArea.style.display = 'none';
+  createPostArea.style.visibility = 'hidden';
 }
 
 shareImageButton.addEventListener('click', openCreatePostModal);
 
 closeCreatePostModalButton.addEventListener('click', closeCreatePostModal);
 
-function createCard() {
+function createCard(card) {
   let cardWrapper = document.createElement('div');
   cardWrapper.className = 'shared-moment-card mdl-card mdl-shadow--2dp';
   let cardTitle = document.createElement('div');
   cardTitle.className = 'mdl-card__title';
-  cardTitle.style.backgroundImage = 'url("/src/images/20181101-barnimages-3.jpg")';
+  let image = new Image();
+  image.src = card.image_id;
+  cardTitle.style.backgroundImage = 'url('+ image.src +')';
   cardTitle.style.backgroundSize = 'cover';
   cardTitle.style.height = '180px';
   cardWrapper.appendChild(cardTitle);
   let cardTitleTextElement = document.createElement('h2');
   cardTitleTextElement.className = 'mdl-card__title-text';
-  cardTitleTextElement.textContent = 'Vor der HTW-Mensa';
+  cardTitleTextElement.textContent = card.title;
   cardTitle.appendChild(cardTitleTextElement);
   let cardSupportingText = document.createElement('div');
   cardSupportingText.className = 'mdl-card__supporting-text';
-  cardSupportingText.textContent = 'HTW Berlin';
+  cardSupportingText.textContent = card.location;
   cardSupportingText.style.textAlign = 'center';
   cardWrapper.appendChild(cardSupportingText);
   componentHandler.upgradeElement(cardWrapper);
   sharedMomentsArea.appendChild(cardWrapper);
 }
 
-fetch('https://jsonplaceholder.typicode.com/posts/1')
+let networkDataReceived = false;
+
+fetch('http://localhost:3000/posts')
     .then((res) => {
       return res.json();
     })
     .then((data) => {
-      createCard();
+      networkDataReceived = true;
+      console.log('From backend ...', data);
+      updateUI(data);
     });
+
+if('indexedDB' in window) {
+  readAllData('posts')
+      .then( data => {
+        if(!networkDataReceived) {
+          console.log('From cache ...', data);
+          updateUI(data);
+        }
+      })
+}
+
+function updateUI(data) {
+  for(let card of data)
+  {
+    createCard(card);
+  }
+}
--- frontend/public/sw.js
+++ frontend/public/sw.js
@@ -1,11 +1,15 @@
-const CURRENT_STATIC_CACHE = 'static-v2';
-const CURRENT_DYNAMIC_CACHE = 'dynamic-v2';
+importScripts('/src/js/idb.js');
+importScripts('/src/js/db.js');
+
+const CACHE_VERSION = 3;
+const CURRENT_STATIC_CACHE = 'static-v'+CACHE_VERSION;
+const CURRENT_DYNAMIC_CACHE = 'dynamic-v'+CACHE_VERSION;
 
 self.addEventListener('install', event => {
     console.log('service worker --> installing ...', event);
     event.waitUntil(
         caches.open(CURRENT_STATIC_CACHE)
-            .then( cache => {
+            .then(cache => {
                 console.log('Service-Worker-Cache erzeugt und offen');
                 cache.addAll([
                     '/',
@@ -13,6 +17,7 @@ self.addEventListener('install', event => {
                     '/src/js/app.js',
                     '/src/js/feed.js',
                     '/src/js/material.min.js',
+                    '/src/js/idb.js',
                     '/src/css/app.css',
                     '/src/css/feed.css',
                     '/src/images/htw.jpg',
@@ -24,13 +29,27 @@ self.addEventListener('install', event => {
     );
 })
 
+const db = idb.openDB('posts-store', 1, {
+    upgrade(db) {
+        // Create a store of objects
+        const store = db.createObjectStore('posts', {
+            // The '_id' property of the object will be the key.
+            keyPath: '_id',
+            // If it isn't explicitly set, create a value by auto incrementing.
+            autoIncrement: true,
+        });
+        // Create an index on the '_id' property of the objects.
+        store.createIndex('_id', '_id');
+    },
+});
+
 self.addEventListener('activate', event => {
     console.log('service worker --> activating ...', event);
     event.waitUntil(
         caches.keys()
-            .then( keyList => {
-                return Promise.all(keyList.map( key => {
-                    if(key !== CURRENT_STATIC_CACHE && key !== CURRENT_DYNAMIC_CACHE) {
+            .then(keyList => {
+                return Promise.all(keyList.map(key => {
+                    if (key !== CURRENT_STATIC_CACHE && key !== CURRENT_DYNAMIC_CACHE) {
                         console.log('service worker --> old cache removed :', key);
                         return caches.delete(key);
                     }
@@ -39,27 +58,47 @@ self.addEventListener('activate', event => {
     );
     return self.clients.claim();
 })
-
 self.addEventListener('fetch', event => {
     // check if request is made by chrome extensions or web page
     // if request is made for web page url must contains http.
     if (!(event.request.url.indexOf('http') === 0)) return; // skip the request. if request is not made with http protocol
 
-    event.respondWith(
-        caches.match(event.request)
-            .then( response => {
-                if(response) {
-                    return response;
-                } else {
-                    return fetch(event.request)
-                        .then( res => {     // nicht erneut response nehmen, haben wir schon
-                            return caches.open(CURRENT_DYNAMIC_CACHE)      // neuer, weiterer Cache namens dynamic
-                                .then( cache => {
-                                    cache.put(event.request.url, res.clone());
-                                    return res;
-                                })
+    const url = 'http://localhost:3000/posts';
+    if(event.request.url.indexOf(url) >= 0) {
+        event.respondWith(
+            fetch(event.request)
+                .then ( res => {
+                    const clonedResponse = res.clone();
+                    clearAllData('posts')
+                        .then( () => {
+                            return clonedResponse.json();
+                        })
+                        .then( data => {
+                            for(let key in data)
+                            {
+                                console.log('write data', data[key]);
+                                writeData('posts', data[key]);
+                            }
                         });
-                }
-            })
-    );
+                    return res;
+                })
+        )
+    } else {
+        event.respondWith(
+            caches.match(event.request)
+                .then( response => {
+                    if(response) {
+                        return response;
+                    } else {
+                        return fetch(event.request)
+                            .then( res => {     // nicht erneut response nehmen, haben wir schon
+                                return caches.open(CURRENT_DYNAMIC_CACHE)      // neuer, weiterer Cache namens dynamic
+                                    .then( cache => {
+                                        cache.put(event.request.url, res.clone());
+                                        return res;
+                                    })
+                            });
+                    }
+                })
+        )}
 })
