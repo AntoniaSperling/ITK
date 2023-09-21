c2f362cc3c862679af8fe6e0cf4b6d70ee022eac
AntoniaSperling <antonia.sperling@stundent.htw.-berlin.de>
2023-09-21 15:32:59 +0200

    Bilder up and download sowie die Speicherung in der DB

--- backend/routes/post.routes.js
+++ backend/routes/post.routes.js
@@ -1,94 +1,126 @@
-const express = require('express');
-const router = express.Router();
-const { collection } = require('../configure/db')
-const  ObjectId = require('mongodb').ObjectId
+const express = require('express')
+const router = express.Router()
+const mongoose = require('mongoose');
+const Post = require('../models/posts');
+const upload = require('../middleware/upload');
+require('dotenv').config();
+
+/* ----------------- POST ---------------------------- */
+
+// POST one post
+router.post('/', upload.single('file'), async(req, res) => {
+    const newPost = new Post({
+        title: req.body.title,
+        location: req.body.location,
+        image_id: req.file.filename
+    })
+    await newPost.save();
+    res.send(newPost);
+});
+
+/* ----------------- GET ---------------------------- */
+
+const credentials = process.env.PATH_TO_PEM
+
+const connection = mongoose.createConnection(process.env.DB_CONNECTION, {
+    sslKey: credentials,
+    sslCert: credentials,
+    dbName: "htwinsta" });
+
+function getOnePost(id) {
+    return new Promise( async(resolve, reject) => {
+        try {
+
+            const post = await Post.findOne({ _id: id });
+            let fileName = post.image_id;
+            const files = connection.collection('posts.files');
+            const chunks = connection.collection('posts.chunks');
+
+            const cursorFiles = files.find({filename: fileName});
+            const allFiles = await cursorFiles.toArray();
+            const cursorChunks = chunks.find({files_id : allFiles[0]._id});
+            const sortedChunks = cursorChunks.sort({n: 1});
+            let fileData = [];
+            for await (const chunk of sortedChunks) {
+                fileData.push(chunk.data.toString('base64'));
+            }
+            let base64file = 'data:' + allFiles[0].contentType + ';base64,' + fileData.join('');
+            let getPost = new Post({
+                "title": post.title,
+                "location": post.location,
+                "image_id": base64file
+            });
+            //console.log('getPost', getPost)
+            resolve(getPost)
+        } catch {
+            reject(new Error("Post does not exist!"));
+        }
+    })
+}
+
+function getAllPosts() {
+    return new Promise( async(resolve, reject) => {
+        const sendAllPosts = [];
+        const allPosts = await Post.find();
+        try {
+            for(const post of allPosts) {
+                console.log('post', post)
+                const onePost = await getOnePost(post._id);
+                sendAllPosts.push(onePost);
+            }
+            console.log('sendAllPosts', sendAllPosts)
+            resolve(sendAllPosts)
+        } catch {
+            reject(new Error("Posts do not exist!"));
+        }
+    });
+}
+
+// GET one post via id
+router.get('/:id', async(req, res) => {
+    getOnePost(req.params.id)
+        .then( (post) => {
+            console.log('post', post);
+            res.send(post);
+        })
+        .catch( () => {
+            res.status(404);
+            res.send({
+                error: "Post does not exist!"
+            });
+        })
+});
 
 // GET all posts
 router.get('/', async(req, res) => {
-    const allPosts = await collection.find().toArray();
-    res.status(200);
-    res.send(allPosts);
+    getAllPosts()
+        .then( (posts) => {
+            res.send(posts);
+        })
+        .catch( () => {
+            res.status(404);
+            res.send({
+                error: "Post do not exist!"
+            });
+        })
 });
 
-// POST one new post
-router.post('/', async(req, res) => {
-
-    try {
-        const newPost = {
-            title: req.body.title,
-            location: req.body.location,
-            image_id: req.body.image_id
-        }
-        const result = await collection.insertOne(newPost);
-        res.status(201);
-        res.send(result);
-    } catch {
-        res.status(404);
-        res.send({
-            error: "Post does not exist!"
-        });
-    }
-});
-
-// GET one post by id
-router.get('/:id', async(req, res) => {
-
-    try {
-        const id_obj = new ObjectId(req.params.id);
-        const post = await collection.find( {_id: id_obj } ).toArray();
-        console.log('post', req.params.id)
-        res.status(202);
-        res.send(post);
-    } catch {
-        res.status(404);
-        res.send({
-            error: "Post does not exist!"
-        });
-    }
-});
-
-// PATCH (update) one post
-router.patch('/:id', async(req, res) => {
-    try {
-        const id_obj = new ObjectId(req.params.id);
-        const post = await collection.findOne({ _id: id_obj })
-
-        if (req.body.title) {
-            post.title = req.body.title
-        }
-
-        if (req.body.location) {
-            post.location = req.body.location
-        }
-
-        if (req.body.image_id) {
-            post.image_id = req.body.image_id
-        }
-
-        await collection.updateOne({ _id: id_obj }, { $set: post });
-        res.send(post)
-    } catch {
-        res.status(404)
-        res.send({ error: "Post does not exist!" })
-    }
-});
+/* ----------------- DELETE ---------------------------- */
 
 // DELETE one post via id
 router.delete('/:id', async(req, res) => {
     try {
-        const id_obj = new ObjectId(req.params.id);
-        const post = await collection.deleteOne({ _id: id_obj })
-        console.log('post', post)
-        if(post.deletedCount === 1) {
-            res.status(204)
-            res.send( { message: "deleted" })
-        } else {
-            res.status(404)
-            res.send({ error: "Post does not exist!" })
-        }
+        const post = await Post.findOne({ _id: req.params.id })
+        let fileName = post.image_id;
+        await Post.deleteOne({ _id: req.params.id });
+        await files.find({filename: fileName}).toArray( async(err, docs) => {
+            await chunks.deleteMany({files_id : docs[0]._id});
+        })
+        await files.deleteOne({filename: fileName});
+        res.status(204).send()
     } catch {
         res.status(404)
-        res.send({ error: "something wrong" })
+        res.send({ error: "Post does not exist!" })
     }
 });
 
