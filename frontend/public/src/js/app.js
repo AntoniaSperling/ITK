c46ac570eac7d8308be68fca91e4926e900445bc
AntoniaSperling <antonia.sperling@stundent.htw.-berlin.de>
2023-09-26 11:50:54 +0200

    Neuer Endpunkt im BE

--- frontend/public/src/js/app.js
+++ frontend/public/src/js/app.js
@@ -35,18 +35,76 @@ function displayConfirmNotification() {
     }
 }
 
+function urlBase64ToUint8Array(base64String) {
+    var padding = '='.repeat((4 - base64String.length % 4) % 4);
+    var base64 = (base64String + padding)
+        .replace(/\-/g, '+')
+        .replace(/_/g, '/');
+
+    var rawData = window.atob(base64);
+    var outputArray = new Uint8Array(rawData.length);
+
+    for (var i = 0; i < rawData.length; ++i) {
+        outputArray[i] = rawData.charCodeAt(i);
+    }
+    return outputArray;
+}
+
+function configurePushSubscription() {
+    if(!('serviceWorker' in navigator)) {
+        return
+    }
+
+    let swReg;
+    navigator.serviceWorker.ready
+        .then( sw => {
+            swReg = sw;
+            return sw.pushManager.getSubscription();
+        })
+        .then( sub => {
+            if (sub === null) {
+                // create a new subscription
+                let vapidPublicKey = 'BGaBbfstZLdgW-p0Cu-1Y4dfUgbY8zWsASyv0ZuO6a7qYAgLgFxodsoUJ7ZsMfk0Ri46qSpNfaSq1smMhijKjzs';
+                let convertedVapidPublicKey = urlBase64ToUint8Array(vapidPublicKey);
+                return swReg.pushManager.subscribe({
+                    userVisibleOnly: true,
+                    applicationServerKey: convertedVapidPublicKey,
+                })
+            } else {
+                // already subscribed
+            }
+        })
+        .then( newSub => {
+            return fetch('http://localhost:3000/subscription', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Accept': 'application/json'
+                },
+                body: JSON.stringify(newSub)
+            })
+                .then( response => {
+                    if(response.ok) {
+                        displayConfirmNotification();
+                    }
+                })
+        });
+}
+
+
 function askForNotificationPermission() {
     Notification.requestPermission( result => {
         console.log('User choice', result);
         if(result !== 'granted') {
             console.log('No notification permission granted');
         } else {
-            displayConfirmNotification();
+            // displayConfirmNotification();
+            configurePushSubscription();
         }
     });
 }
 
-if('Notification' in window) {
+if('Notification' in window && 'serviceWorker' in navigator) {
     for(let button of enableNotificationsButtons) {
         button.style.display = 'inline-block';
         button.addEventListener('click', askForNotificationPermission);
