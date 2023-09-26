12fb294dc97a87bbcc76d57455811d084e45a5ec
AntoniaSperling <antonia.sperling@stundent.htw.-berlin.de>
2023-09-26 09:12:33 +0200

    Benachrichtigungen anzeigen

--- frontend/public/src/js/app.js
+++ frontend/public/src/js/app.js
@@ -1,3 +1,5 @@
+let enableNotificationsButtons = document.querySelectorAll('.enable-notifications');
+
 if ('serviceWorker' in navigator) {
     navigator.serviceWorker
         .register('/sw.js')
@@ -7,4 +9,46 @@ if ('serviceWorker' in navigator) {
         .catch(
             err => { console.log(err); }
         );
+}
+
+function displayConfirmNotification() {
+    if('serviceWorker' in navigator) {
+        let options = {
+            body: 'You successfully subscribed to our Notification service!',
+            icon: '/src/images/icons/image_96x96.png',
+            image: '/src/images/foto-sm.jpg',
+            lang: 'de-DE',
+            vibrate: [100, 50, 200],
+            badge: '/src/images/icons/image_96x96.png',
+            tag: 'confirm-notification',
+            renotify: true,
+            actions: [
+                { action: 'confirm', title: 'Ok', icon: '/src/images/icons/image_96x96.png' },
+                { action: 'cancel', title: 'Cancel', icon: '/src/images/icons/image_96x96.png' },
+            ]
+        };
+
+        navigator.serviceWorker.ready
+            .then( sw => {
+                sw.showNotification('Successfully subscribed (from SW)!', options);
+            });
+    }
+}
+
+function askForNotificationPermission() {
+    Notification.requestPermission( result => {
+        console.log('User choice', result);
+        if(result !== 'granted') {
+            console.log('No notification permission granted');
+        } else {
+            displayConfirmNotification();
+        }
+    });
+}
+
+if('Notification' in window) {
+    for(let button of enableNotificationsButtons) {
+        button.style.display = 'inline-block';
+        button.addEventListener('click', askForNotificationPermission);
+    }
 }
